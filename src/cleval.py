53b3a6e45944da699ca8499bb709c4c25e7e69b1
mwestera <m.westera@hum.leidenuniv.nl>
2025-02-06 22:06:12 +0100

    enable multilabel

--- src/cleval.py
+++ src/cleval.py
@@ -2,7 +2,9 @@ import sys
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.metrics import confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, classification_report, multilabel_confusion_matrix
+from sklearn.preprocessing import MultiLabelBinarizer
+
 import argparse
 from io import BytesIO
 
@@ -12,14 +14,13 @@ from reportlab.lib.utils import ImageReader
 
 import logging
 import json
+import csv
 
-logger = logging.getLogger('cleval')
-handler = logging.StreamHandler(sys.stderr)
-handler.setFormatter(logging.Formatter(''))
-handler.setLevel(logging.INFO)
+import random
 
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
+import itertools
+
+logging.basicConfig(level=logging.INFO, format='')
 
 
 """
@@ -39,11 +40,15 @@ $ cat preds.txt | cleval - targs.txt --pdf report.pdf > metrics.json
 """
 
 
+# TODO: Second choices, proba, and write examples to html/pdf
 
 def main():
     parser = argparse.ArgumentParser(description='Compare precomputed lists of predictions vs targets, logging a simple classification report, optionally PDF, and outputting json.')
     parser.add_argument('true', type=argparse.FileType('r'), help='File containing predicted classes, or - for stdin.')
     parser.add_argument('pred', type=argparse.FileType('r'), nargs='?', default=None, help='File containing true classes, or - for stdin; default None, exploring only predictions.')
+    parser.add_argument('--multi', action='store_true', help='To assume potentially multiple labels per item (multi-label classification), in which case true and pred files contain csv rows; inferred from true otherwise.')
+    parser.add_argument('--prob', action='store_true', help='If pred file contains raw probabilities instead of labels; inferred from pred otherwise.')
+    parser.add_argument('--labels', nargs='*', type=str, default=[], help='Classification labels (separated by spaces); inferred from true otherwise.')
     parser.add_argument('--pdf', type=str, default=None, help='Path to write PDF report to.')
 
     args = parser.parse_args()
@@ -51,33 +56,78 @@ def main():
     file_true = sys.stdin if args.true == '-' else args.true
     file_pred = sys.stdin if args.pred == '-' else args.pred
 
-    y_true = [line.strip() for line in file_true]
-    y_pred = [line.strip() for line in file_pred] if file_pred else y_true  # TODO: Rather set to None, and make evaluate() handle it...
+    y_true = [set(row) for row in csv.reader(file_true)]
+    y_pred = [set(row) for row in csv.reader(file_pred)] if file_pred else None
 
-    result = evaluate(y_pred, y_true, args.pdf)
+    # if only "but" was a keyword...
+    if not args.multi and any(len(row) > 1 for row in y_true) or any(len(row) > 1 for row in y_pred):
+        logging.warning('--multi is inferred.')
+        args.multi = True
+
+    if not args.multi:
+        y_true = list(itertools.chain(y_true))  # flatten all the singletons
+        y_pred = list(itertools.chain(y_pred))
+
+    if not args.prob and all(isinstance(i, float) for row in y_pred for i in row):
+        logging.warning('--prob is inferred.')
+        args.prob = True
+
+    if not args.labels:
+        args.labels = sorted(set(y_true)) if not args.multi else sorted(set(itertools.chain(*y_true)))
+        logging.warning(f'--labels are inferred: {args.labels}')
+
+    result = evaluate(y_true, y_pred, labels=args.labels, is_multilabel=args.multi, is_probs=args.prob, output_pdf=args.pdf)
 
     print(json.dumps(result))
 
 
 
-def evaluate(y_pred, y_true, output_pdf=None) -> dict:
+def evaluate(y_true_raw, y_pred_raw, labels, is_multilabel=False, is_probs=False, output_pdf=None) -> dict:
 
-    labels = list(sorted(set(y_pred) & set(y_pred)))
+    if y_pred_raw is None:
+        y_pred_raw = y_true_raw
 
-    logger.info(classification_report(y_true, y_pred))
+    # TODO handle is_probs
+
+    if is_multilabel:
+        mlb = MultiLabelBinarizer()
+        y_true, y_pred = collapse_multilabel(y_true_raw, y_pred_raw)
+        y_true_report = mlb.fit_transform(y_true_raw)
+        y_pred_report = mlb.transform(y_pred_raw)
+    else:
+        y_true_report, y_pred_report = y_true, y_pred = y_true_raw, y_pred_raw
+
+    report = classification_report(y_true_report, y_pred_report, target_names=labels)
+    logging.info(report)
 
     conf_matrix = confusion_matrix(y_true, y_pred, labels=labels)
-    logger.info('Confusion table:\n')
+    logging.info('Confusion table:\n')
+
     conf_matrix_df = pd.DataFrame(conf_matrix, index=labels, columns=labels)
-    conf_matrix_df.index.name = 'True'
-    conf_matrix_df.columns.name = 'Predicted'
-    logger.info('                Predicted:')
-    logger.info(conf_matrix_df.to_markdown())
+    conf_matrix_df.index.name = 'True:'
+    conf_matrix_df.columns.name = 'Predicted:'
+    logging.info(conf_matrix_df)
 
     if output_pdf:
         write_pdf_report(y_true, y_pred, conf_matrix, labels, output_pdf)
 
-    return classification_report(y_true, y_pred, output_dict=True)
+    return classification_report(y_true_report, y_pred_report, target_names=labels, output_dict=True)
+
+
+def collapse_multilabel(y_true, y_pred):
+    """
+    >>> collapse_multilabel([{1, 2}, {2, 3, 4}], [{1, 3}, {4, 5}])
+    ([1, 2, 4, 3], [1, 3, 4, 5])
+    """
+    new_items = []
+    for t, p in zip(y_true, y_pred):
+        t_minus_p = sorted(a for a in t if a not in p)
+        p_minus_t = sorted(a for a in p if a not in t)
+        for l in t & p:
+            new_items.append((l, l))
+        for l1, l2 in itertools.product(t_minus_p, p_minus_t):
+            new_items.append((l1, l2))
+    return tuple(map(list, zip(*new_items)))
 
 
 def write_pdf_report(y_true, y_pred, conf_matrix, labels, out_path) -> None:
@@ -127,14 +177,6 @@ def write_pdf_report(y_true, y_pred, conf_matrix, labels, out_path) -> None:
     c.showPage()
     c.save()
     logger.info(f"\nPDF report saved as {pdf_filename}")
-
-
-def read_input(file):
-    if file == '-':
-        return [line.strip() for line in sys.stdin.readlines()]
-    else:
-        with open(file, 'r') as f:
-            return [line.strip() for line in f.readlines()]
 
 
 
